374756a425dba6382d9c5fa2d7979aa18dda5bdb
balogunkeji <tomitoyo.93@gmail.com>
2020-08-04 18:32:35 +0100

    first commit

---
+++ js/style.js
@@ -0,0 +1,33 @@
+const toggleSwitch = document.querySelector('.switch');
+
+
+toggleSwitch.addEventListener('click', switchButton, false);
+
+function switchButton(e){
+    if(e.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else{
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+function switchButton(e){
+    if(e.target.checked){
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme','light');
+    } 
+    else{
+        document.documentElement.setAttribute('data-theme', 'dark');
+        window.localStorage.setItem('theme', JSON.stringify('dark'));
+    }
+}
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', 'currentTheme');
+
+    if (currentTheme === 'light'){
+
+        toggleSwitch.checked = true;
+    }
+}
