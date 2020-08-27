ea7f8b4dff63ebfbc5ec94d44b860707302db8cc
balogunkeji <tomitoyo.93@gmail.com>
2020-08-27 06:37:36 +0100

    first commit

--- js/style.js
+++ js/style.js
@@ -1,36 +1,36 @@
 AOS.init();
 
 
-const toggleSwitch = document.querySelector('.dropdown-content');
+const toggleSwitch = document.querySelector('.dropdown-content a', '[data-theme]');
 
 
-toggleSwitch.addEventListener('click', switchButton, false);
+toggleSwitch.addEventListener('click', switchButton);
 
 function switchButton(e){
-    if(e.target.checked){
-        document.documentElement.setAttribute('data-theme', 'dark');
+    if(e.target.click){
+        document.documentElement.setAttribute('data-theme','light');
     } else{
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
-}
-
-function switchButton(e){
-    if(e.target.checked){
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme','light');
-    } 
-    else{
         document.documentElement.setAttribute('data-theme', 'dark');
-        window.localStorage.setItem('theme', JSON.stringify('dark'));
     }
 }
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', 'currentTheme');
+// function switchButton(e){
+//     if(e.target.checked){
+//         document.documentElement.setAttribute('data-theme', 'light');
+//         localStorage.setItem('theme','light');
+//     } 
+//     else{
+//         document.documentElement.setAttribute('data-theme', 'dark');
+//         window.localStorage.setItem('theme', JSON.stringify('dark'));
+//     }
+// }
+// const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-    if (currentTheme === 'light'){
+// if (currentTheme) {
+//     document.documentElement.setAttribute('data-theme', 'currentTheme');
 
-        toggleSwitch.checked = true;
-    }
-}
+//     if (currentTheme === 'light'){
+
+        // toggleSwitch.checked = true;
+//     }
+// }
